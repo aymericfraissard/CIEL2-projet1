84f9e0bfbc1e186849568b80998e01c975da05de
Quentin-cruz <quentin-cruz@outlook.fr>
2026-09-22 11:30:46 +0200

    bases fonctions

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require('express');
+const app = express();
 const port = 3001;
+const mysql = require('mysql2');
+require('dotenv').config();
+const IPServer = process.env.IPServer;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.end('Voilà la réponse du serveur !\n');
+const connection = mysql.createConnection({ // configuration de la connexion à la base de données
+  host: process.env.ipBDD, //changer ip
+  user: process.env.LoginBDD,
+  password: process.env.PasswordBDD,
+  database: process.env.DatabaseBDD
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Serveur en ligne sur http://${hostname}:${port}/`);
+connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données :', err);
+    return;
+  }
+  console.log('Connecté à la base de données MySQL.');
+});
+
+app.use(express.json());
+app.use(express.static('public'));
+
+function inscription(login, passwd) {
+  
+}
+
+app.listen(port, IPServer, () => {
+  console.log(`Serveur en ligne sur http://${IPServer}:${port}/`);
 });
